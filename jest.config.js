bd8df06fa907c3c81dc45699b0780ba02209822c
has9800 <hahmed28@nait.ca>
2022-09-25 10:28:46 -0600

    test: add playwright

--- jest.config.js
+++ jest.config.js
@@ -20,7 +20,11 @@ module.exports = {
     '^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$':
       '<rootDir>/__mocks__/fileMock.js',
   },
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/.next/',
+    '<rootDir>/__tests__/e2e/',
+  ],
   testEnvironment: 'jsdom',
   transform: {
     /* Use babel-jest to transpile tests with the next/babel preset
